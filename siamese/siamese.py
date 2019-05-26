f38f4bee80591359864ade937956a63ee8cdc1e3
Harry <himion0@gmail.com>
2019-05-26 12:33:16 +0100

    Run out of memory attempt

--- siamese/siamese.py
+++ siamese/siamese.py
@@ -1,6 +1,7 @@
-from fastai.basics import ItemBase, ItemList, LabelList, CategoryList, Dataset
+from fastai.basics import ItemBase, ItemList, LabelList, LabelLists, CategoryList, Dataset
 from copy import deepcopy
 import numpy as np
+import torch
 
 
 class ItemTuple(ItemBase):
@@ -22,53 +23,104 @@ class ItemTuple(ItemBase):
         return self
 
     def __repr__(self):
-        return ''.join([str(x) for x in self.items]) + '\n'
+        return ''.join([str(x)+'\n' for x in self.items])
 
     def __len__(self):
         return self.size
 
 
-class SiameseList(ItemList):
 
-    def __init__(self, *args, open_fn=lambda x: x, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.open = open_fn
-
-    def get(self, i):
-
-        # i is an integer then we convert to 2d
-        item = super().get(i)
-        if isinstance(item, ItemTuple):
-            return item
-        
-        return ItemTuple([self.open(x) for x in item])
-
+def _make_ll(ll: LabelList, pct_same=.5, tar_num=None, **kwargs):
+    x = ll.x
+    y = ll.y
+    seperated = [x.items[y.items == i] for i in range(ll.c)]
+    pairs = gen_pairs(seperated, pct_same, tar_num)
+    # Keeping in sorted form
+    ll.x.items = np.concatenate(seperated)
+    ll.y.items = np.concatenate(
+        [y.items[y.items == i] for i in range(ll.c)])
+    ret = SiameseDataset(ll.x, ll.y, **kwargs)
+    ret.pairs = pairs
+    ret.lens = [len(i) for i in seperated]
+    return ret
 
 class SiameseDataset(LabelList):
-
+    
     @classmethod
-    def create_from_ll(cls, ll: LabelList, bs: int = 1, pct_same=0.5):
-        x = ll.x
-        y = ll.y
-        seperated = [x.items[y == i] for i in range(ll.c)]
+    def create_from_ll(cls, ll: LabelList, pct_same=0.5, tar_num=None, split_c=None, split_pct=.2, **kwargs):
+        if tar_num is None:
+            tar_num = len(ll.x.items)
 
-    def __len__(self);
-        return len(self.x,items)
+        if isinstance(split_c, float):
+            split_c = list(range(int(ll.c*split_c)))
+        
+        mask = None
+        for i in split_c:
+            if mask is None:
+                mask = ll.y.items == i
+            else:
+                mask = np.logical_or(mask, ll.y.items == i)
+        tll, vll = deepcopy(ll), deepcopy(ll)
+        tll.x.items = tll.x.items[~mask]
+        tll.y.items = tll.y.items[~mask]
+        vll.x.items = vll.x.items[mask]
+        vll.y.items = vll.y.items[mask]
+        train = _make_ll(tll, pct_same=pct_same, tar_num=tar_num*(1-split_pct), **kwargs)
+        valid = _make_ll(vll, pct_same=pct_same, tar_num=tar_num*split_pct, **kwargs)
+        return LabelLists(ll.path, train, valid)
+
+    def _same(self, items):
+        prev = None
+        for i in items:
+            c = self._get_class(i)
+            if prev is not None and prev != c:
+                return False
+            prev = c
+        return True
+
+    def _get_class(self, idx):
+        total = 0
+        for i, l in enumerate(self.lens):
+            total += l
+            if idx < total+l:
+                return i
+
+    def __len__(self):
+        return len(self.pairs)
+
+    def totuple(self, i):
+        x = self.pairs[idxs]
+        items = [self.x[i] for i in x]
+        y = 0 if self._same(x) else 1
+        x = ItemTuple(items)
+        return x, y
+
+    def __getitem__(self, idxs):
+        if not isinstance(idxs, int):
+            print("HELP!")
+        x = self.pairs[idxs]
+        items = [self.x[i] for i in x]
+        y = 0 if self._same(x) else 1
+        x = ItemTuple(items)
+        return x, y
 
 
-"""
-Questions
-- How do we make sure there is a percentage of similar pairs?
-- Ensure that everything is paired up atleast once?
-- Is there a mathemtical way to do this?
+def gen_pairs(classes_arr, pct_same, total_pairs):
+    """
+    Creates a matrix of possible unique pairs and applies probabilities
+    based on target total of items required.
+    TODO Guaruntee one combination of each class
+    TODO Add custom pairs to generated pairs
+    """
+    Sn = sum([len(I) for I in classes_arr])
+    add_chance = 2*total_pairs / (Sn*(Sn+1)/2)
+    s = Sn, Sn
+    print(s)
+    M = torch.rand(s).tril.t() < (1-pct_same) * add_chance
+    i = 0
+    for I in classes_arr:
+        l = len(I)
+        R[i: i+l, i: i+l] = torch.rand() < pct_same * add_chance
+        i += l
 
-
-- Specify the number of total pairs
-- Percentage the same, (implying percentage different)
-- 
-
-1. Get given a labelled list of items with paramaters
-2. Generate from that add the indexs of the pairs as if the items had been stacked up against each other
-    a1 , ... , an , b1 , ... , bm ,
-3. 
-"""
+    return (M*R).nonzero()
--- test.py
+++ test.py
@@ -1,1 +1,23 @@
 from siamese import *
+import sys
+sys.path.append("../audio")
+from audio import *
+from siamese import *
+np.random.seed(2)
+
+data_url = 'http://www.openslr.org/resources/45/ST-AEDS-20180100_1-OS'
+data_folder = datapath4file(url2name(data_url))
+untar_data(data_url, dest=data_folder)
+label_pattern = r'_/([mf]\d+)_'
+config = AudioTransformConfig()
+config.segment_size = 1000
+# config.remove_silence = True
+config.f_max = 8000
+config.to_db_scale = True
+config.top_db = 80
+# config.silence_threshold = 30
+config.max_to_pad = 1000
+# config.silence_padding = 300
+audios = AudioList.from_folder(data_folder, config=config).split_none().label_from_re(label_pattern)
+sds = SiameseDataset.create_from_ll(audios, split_c=.2, tar_num=10000)
+ex = sds.train[0]
